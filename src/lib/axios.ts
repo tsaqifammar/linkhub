337d512e97816c267aa059a8ebc8c5dd10dfdc38
Muhammad Tsaqif Ammar <tsaqifammarmuh@gmail.com>
2023-03-12 13:37:24 +0700

    fix: axios url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const url = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : "http://localhost:3000";
+const url = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 const axiosInstance = axios.create({
   baseURL: url + "/api",
